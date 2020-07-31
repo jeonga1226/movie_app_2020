a0f43deb8c7e4592b70d7a6391e5b3e6b84a1006
uracle <uracle@uracleui-MacBookPro.local>
2020-07-31 17:14:31 +0900

    Updates

---
+++ precache-manifest.4894cc20eb51a8f97e0db8c2f5030649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7410f465427eda5f83b129e719d12311",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "37e9a7bb0b80efd48cd1",
+    "url": "/movie_app_2020/static/css/main.06a47d50.chunk.css"
+  },
+  {
+    "revision": "644879ec28dcd1aaa90d",
+    "url": "/movie_app_2020/static/js/2.26481c6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.26481c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e9a7bb0b80efd48cd1",
+    "url": "/movie_app_2020/static/js/main.db4354ca.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
